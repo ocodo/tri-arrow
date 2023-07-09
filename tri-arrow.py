35dfec8a698637668b37373a9fbc93c9d45ea4d3
jasonm23 <jasonm23@gmail.com>
2023-07-09 07:16:11 +0700

    Fix parametric setting of row/col and scale up.

--- tri-arrow.py
+++ tri-arrow.py
@@ -2,10 +2,10 @@
 
 from turtle import *
 
-columns = 26
-rows = 24
+columns = 13
+rows = 12
 
-scale = 1.8
+scale = 2.1
 tracer(0,0)
 speed(0)
 hideturtle()
@@ -18,14 +18,14 @@ left(90)
 forward(45 * 11)
 right(90)
 
-for a in range(columns):
-    for b in range(rows):
-        ch = (b + 1)/25
-        fill_color = (ch/3 + ((a/columns)/1.6), ch/2 + 0.2, 0.7)
-        stroke_color = (ch/3 + ((a/columns)/1.6),ch/2 + 0.21, 0.8)
+for col in range(columns):
+    for row in range(rows):
+        ch = (row + 1)/25
+        fill_color = (ch/3 + ((col/columns)/1.6), ch/2 + 0.2, 0.7)
+        stroke_color = (ch/3 + ((col/columns)/1.6),ch/2 + 0.21, 0.8)
         color(stroke_color, fill_color)
         begin_fill()
-        for c in range(3):
+        for arrow in range(3):
             pendown()
             forward(25 * scale)
             left(120)
@@ -47,10 +47,10 @@ for a in range(columns):
         end_fill()
         penup()
         forward(45 * scale)
-    if a % 2 == 0:
-        reverse = columns - 1
+    if col % 2 == 0:
+        reverse = rows + 1
     else:
-        reverse = columns - 2
+        reverse = rows
 
     backward(45 * scale * reverse)
     right(60)
